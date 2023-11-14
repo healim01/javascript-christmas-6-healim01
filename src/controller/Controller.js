dff2afab8df5cb67d3f21135bbccf1dd4b51fcbb
Hyelim Choi <HyelimChoi01@gmail.com>
2023-11-14 13:12:27 +0900

    feat: get customer's menu

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,19 +1,31 @@
-import MESSAGES from "../constants/messages.js";
+import splitStringToArray from "../utils/splitStringtoArray.js";
 import InputView from "../view/InputView.js";
 
 class Controller {
   #rDate;
+  #rMenu;
 
   constructor() {
     this.#rDate = 0;
+    this.#rMenu = {};
   }
   async start() {
     this.#constomerInfo();
   }
 
   async #constomerInfo() {
-    console.log(MESSAGES.welcome);
+    InputView.welcome();
+    await this.#getDate();
+    this.#rMenu = await this.#getMenu();
+  }
+
+  async #getDate() {
     this.#rDate = await InputView.readDate();
+  }
+
+  async #getMenu() {
+    const iMenu = await InputView.readMenu();
+    return splitStringToArray(iMenu);
   }
 }
 export default Controller;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,8 +2,18 @@ import { Console } from "@woowacourse/mission-utils";
 import MESSAGES from "../constants/messages.js";
 
 const InputView = {
+  welcome() {
+    Console.print(MESSAGES.welcome);
+  },
+
   async readDate() {
     const input = await Console.readLineAsync(MESSAGES.readDate);
+    return input;
+  },
+
+  async readMenu() {
+    const input = await Console.readLineAsync(MESSAGES.readMenu);
+    return input;
   },
 };
 export default InputView;
